960a45dad8c32b4ba3c24d6b1eff7dca71b7c935
deckbsd <deckbsd@protonmail.com>
2020-06-30 10:07:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Whoun
+
+This little node js app has been made to know who unfollewed a twitter account WITHOUT using the twitter API.
+
+## Installation
+
+Clone the repository and than simply run :
+```bash
+$ npm install
+```
+
+## Running the code
+
+Without viewing the browser :
+
+```bash
+$ node app.js my_twitter_account my_twitter_password twitter_account_i_want_to_check
+```
+
+To display the browser during the process, just add v a the end of the command line :
+
+```bash
+$ node app.js my_twitter_account my_twitter_password twitter_account_i_want_to_check v
+```
+
+You have to run it a first time for the program to create a first list of pseudos. From there the program will start comparing the first list and the new list to find unfollowers. Once finished, the new pseudo list is saved in the pseudo file (the existing one is overrided).
+
+## Be aware of
+
+    * This program is based on the Twitter html source code. So if, in the futur the structure change, there is a possibility that the program will stop working. To avoid that, i didn't use any css class names (This a element that is subject to change often) in the part that extracts the pseudos. But still, it can happen.
+    * Sometimes the program may miss loading the good page or make silly things (due to the speed of the internet connections, used cpu ressources, ...). To see what's wrong when navigating, you can use the 'v' command line argument. You can also change the delay in the code to adapt it to your context.
---
+++ app.js
@@ -0,0 +1,133 @@
+const puppeteer = require('puppeteer')
+const htmlparser = require("htmlparser2")
+const fs = require("fs")
+
+let pseudos = []
+
+async function loginTweeter(page, twitter_account, password) {
+    try {
+        /** Define twitter fields */
+        let twitterAccount = {
+            userField: "input[name='session[username_or_email]']",
+            passField: "input[name='session[password]']",
+            loginSubmit: ".EdgeButton"
+        };
+
+        await page.waitFor(5000)
+
+        await page.waitForSelector(twitterAccount.userField)
+        await page.click(twitterAccount.userField)
+        await page.keyboard.type(twitter_account)
+
+        await page.waitForSelector(twitterAccount.passField)
+        await page.click(twitterAccount.passField)
+        await page.keyboard.type(password)
+
+        await page.waitFor(2000)
+
+        await page.click('div[data-testid="LoginForm_Login_Button"]')
+    } catch (err) {
+        console.log(err)
+        return err
+    }
+}
+
+async function get_links(page){
+    let bodyHTML = await page.content()
+    let section = false
+    let a = false
+    const parser = new htmlparser.Parser({
+        onopentag: (name, attrib) => {
+            if(name === "section" && attrib.role != undefined && attrib.role === "region"){
+                section = true
+            }
+
+            if(name === "a" && attrib.dir != undefined && attrib.dir === "ltr" && section === true){
+                a = true
+            }
+        },
+        ontext: (text) => {
+            if(text.startsWith('@') && section === true && a === false)
+                if (!pseudos.includes(text))
+                    pseudos.push(text)
+        },
+        onclosetag: (name, attrib) => {
+            if(name === "section" && section === true){
+                section = false
+            }
+
+            if(name === "a" && a === true &&section === true){
+                a = false
+            }
+        },
+    }, {decodeEntities: true})
+    parser.write(bodyHTML)
+    parser.end()
+}
+
+async function scrollToBottom(page) {
+    const distance = 100; // should be less than or equal to window.innerHeight
+    const delay = 100;
+    while (await page.evaluate(() => document.scrollingElement.scrollTop + window.innerHeight < document.scrollingElement.scrollHeight)) {
+        await page.evaluate((y) => { document.scrollingElement.scrollBy(0, y) }, distance)
+        await page.waitFor(delay)
+        get_links(page)
+    }
+}
+
+async function run(){
+    if(process.argv.length < 5){
+        console.log("Missing arguments  [account password account_to_check]")
+        process.exit(1)
+    }
+
+    let headless = true
+    let twitter_account = process.argv[2]
+    let password = process.argv[3]
+    let account_to_check = process.argv[4]
+
+    if(process.argv.length == 6 && process.argv[5] == 'v'){
+        headless = false
+    }
+    
+    let filename = "pseudos_" + account_to_check + ".txt"
+    let previous_pseudos = null
+    try {
+        let file_content = fs.readFileSync(filename, "utf8");
+        previous_pseudos = file_content.split(/\n|\r/g);
+    }catch(err){
+        console.log("No pseudos file found. A first one will be created")
+    }
+
+    const browser = await puppeteer.launch({headless: headless})
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1280, height: 800 })
+
+    console.log("Connecting to twitter...")
+    await page.goto('https://twitter.com')
+    await loginTweeter(page, twitter_account, password)
+    await page.waitFor(3000)
+
+    console.log("Connecting https://twitter.com/" + account_to_check + "/followers...")
+    await page.goto("https://twitter.com/" + account_to_check + "/followers")
+    await page.waitFor(5000)
+
+    console.log("Scrolling the followers page (this can take times)...")
+    await scrollToBottom(page)
+
+    console.log("Found : " + pseudos.length + " followers")
+    file = fs.createWriteStream(filename);
+    file.on('error', function(err) { console.log("Cannot open the file") })
+    pseudos.forEach(function(v) { file.write(v + '\n') })
+    file.end()
+
+    console.log("Unfollower(s) : ")
+    if(previous_pseudos !== null){
+        let result = previous_pseudos.filter(e => !pseudos.find(a => e === a));
+        console.log(result)
+    }
+
+    await browser.close()
+}
+
+run()
